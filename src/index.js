ec0475e24c0c8823d7b9d210948c2be2a5c4b657
Danny Evins <dcastkid@gmail.com>
2016-07-06 08:06:16 -0500

    using hard coded string for now

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import netflixResult from 'netflixResult';
 
 export default function (parent) {
-  const query = parent.querySelector('search__bar').value;
+  // const query = parent.querySelector('search__bar').value;
+  const query = 'attack on titan';
   netflixResult(parent, query);
 }
