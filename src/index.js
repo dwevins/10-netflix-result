e2fd27dcf4c187ef24f1279775abde4f01c80392
Danny Evins <dcastkid@gmail.com>
2016-07-06 08:22:32 -0500

    add listener for search button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import netflixResult from 'netflixResult';
 
 export default function (parent) {
   // const query = parent.querySelector('search__bar').value;
-  const query = 'attack on titan';
-  netflixResult(parent, query);
+
+  const searchBtn = parent.querySelector('searchBtn');
+
+  function update() {
+    const query = 'attack on titan';
+    netflixResult(parent, query);
+  }
+
+  searchBtn.addEventListener('click', update);
 }
