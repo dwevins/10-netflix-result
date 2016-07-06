2b482dfe5925388b7336cfd54ff3027fe4b60c32
Danny Evins <dcastkid@gmail.com>
2016-07-06 08:19:11 -0500

    add fetch logic, testing with console.log()

--- src/netflixResult.js
+++ src/netflixResult.js
@@ -1,5 +1,9 @@
 'use strict';
 
 export default function (parent, query) {
-
+  const movieFetch = fetch(`http://netflixroulette.net/api/api.php?title=${query}`);
+  const movieJson = movieFetch.then((res) => res.json());
+  movieJson.then((data) => {
+    console.log(data);
+  });
 }
