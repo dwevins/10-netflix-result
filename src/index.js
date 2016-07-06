194ab24f69c23bb437e65206a311dd8e099030be
Danny Evins <dcastkid@gmail.com>
2016-07-06 08:27:22 -0500

    fix error passing elements from html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import netflixResult from 'netflixResult';
 export default function (parent) {
   // const query = parent.querySelector('search__bar').value;
 
-  const searchBtn = parent.querySelector('searchBtn');
+  const searchBtn = parent.querySelector('.search__btn');
 
   function update() {
     const query = 'attack on titan';
